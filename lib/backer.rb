821e82cb436b8282f0ad5cb23b0841500d89e1b9
Jack Goode <jack.w.goode@icloud.com>
2020-06-25 00:11:54 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+  attr_reader :backed_projects, :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    add_project(project)
+    project.backers << self
+  end
+  
+  def add_project(project) 
+    @backed_projects << project
+  end
+  
+  
+end
